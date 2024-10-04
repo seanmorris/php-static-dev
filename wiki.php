0ac32d41ceb3b17d1b9b386861e03c9631481a84
Sean Morris <640101+seanmorris@users.noreply.github.com>
2024-10-04 12:15:12 -0400

    test

--- wiki.php
+++ wiki.php
@@ -1,7 +1,7 @@
 <?php
 $fetch = (new Vrzno)->fetch(
 	'https://jsonplaceholder.typicode.com/posts/1',
-	// (object)['headers' => (object)[ 'user-agent' => 'google-chrome lol' ]]
+	(object)['headers' => (object)[ 'user-agent' => 'google-chrome lol' ]]
 	// (object)['method' => 'GET' ]
 );
 
